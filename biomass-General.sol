3d3420f92095408d906aa91b66b0a2288f9bc02c
Oscarts <Oscarts>
2014-10-11 11:20:43 +0200

    go

---
+++ biomass-General.sol
@@ -0,0 +1,179 @@
+---------------------------------------------------------------------------------------------------------
+Problem              /Users/oscar/Documents/Biomass_last_version/biomass-General.cmpl
+Nr. of variables     90
+Nr. of constraints   67
+Objective name       Profit
+Solver name          CBC
+Display variables    (all)
+Display constraints  (all)
+---------------------------------------------------------------------------------------------------------
+
+Objective status     optimal
+Objective value      19 (max!)
+
+Variables 
+Name                 Type            Activity         Lower bound         Upper bound            Marginal
+---------------------------------------------------------------------------------------------------------
+X[F_1]                  C                   0                   0            Infinity                   -
+X[F_2]                  C                   0                   0            Infinity                   -
+X[F_3]                  C                   0                   0            Infinity                   -
+X[F_4]                  C                   9                   0            Infinity                   -
+X[F_5]                  C                  14                   0            Infinity                   -
+X[F_6]                  C                   0                   0            Infinity                   -
+XS[F_1,F_1]             C                   0                   0            Infinity                   -
+XS[F_1,F_2]             C                   0                   0            Infinity                   -
+XS[F_1,F_3]             C                   0                   0            Infinity                   -
+XS[F_1,F_4]             C                   0                   0            Infinity                   -
+XS[F_1,F_5]             C                   0                   0            Infinity                   -
+XS[F_1,F_6]             C                   0                   0            Infinity                   -
+XS[F_2,F_1]             C                   0                   0            Infinity                   -
+XS[F_2,F_2]             C                   0                   0            Infinity                   -
+XS[F_2,F_3]             C                   0                   0            Infinity                   -
+XS[F_2,F_4]             C                   0                   0            Infinity                   -
+XS[F_2,F_5]             C                   0                   0            Infinity                   -
+XS[F_2,F_6]             C                   0                   0            Infinity                   -
+XS[F_3,F_1]             C                   0                   0            Infinity                   -
+XS[F_3,F_2]             C                   0                   0            Infinity                   -
+XS[F_3,F_3]             C                   0                   0            Infinity                   -
+XS[F_3,F_4]             C                   0                   0            Infinity                   -
+XS[F_3,F_5]             C                   0                   0            Infinity                   -
+XS[F_3,F_6]             C                   0                   0            Infinity                   -
+XS[F_4,F_1]             C                   0                   0            Infinity                   -
+XS[F_4,F_2]             C                   0                   0            Infinity                   -
+XS[F_4,F_3]             C                   0                   0            Infinity                   -
+XS[F_4,F_4]             C                   8                   0            Infinity                   -
+XS[F_4,F_5]             C                   0                   0            Infinity                   -
+XS[F_4,F_6]             C                   1                   0            Infinity                   -
+XS[F_5,F_1]             C                   4                   0            Infinity                   -
+XS[F_5,F_2]             C                   5                   0            Infinity                   -
+XS[F_5,F_3]             C                   3                   0            Infinity                   -
+XS[F_5,F_4]             C                   0                   0            Infinity                   -
+XS[F_5,F_5]             C                   2                   0            Infinity                   -
+XS[F_5,F_6]             C                   0                   0            Infinity                   -
+XS[F_6,F_1]             C                   0                   0            Infinity                   -
+XS[F_6,F_2]             C                   0                   0            Infinity                   -
+XS[F_6,F_3]             C                   0                   0            Infinity                   -
+XS[F_6,F_4]             C                   0                   0            Infinity                   -
+XS[F_6,F_5]             C                   0                   0            Infinity                   -
+XS[F_6,F_6]             C                   0                   0            Infinity                   -
+Y[F_1,small]            B                   0                   0                   1                   -
+Y[F_1,big]              B                   0                   0                   1                   -
+Y[F_2,small]            B                   0                   0                   1                   -
+Y[F_2,big]              B                   0                   0                   1                   -
+Y[F_3,small]            B                   0                   0                   1                   -
+Y[F_3,big]              B                   0                   0                   1                   -
+Y[F_4,small]            B                   1                   0                   1                   -
+Y[F_4,big]              B                   0                   0                   1                   -
+Y[F_5,small]            B                   1                   0                   1                   -
+Y[F_5,big]              B                   0                   0                   1                   -
+Y[F_6,small]            B                   0                   0                   1                   -
+Y[F_6,big]              B                   0                   0                   1                   -
+W[F_1,F_1]              C                   0                   0            Infinity                   -
+W[F_1,F_2]              C                   0                   0            Infinity                   -
+W[F_1,F_3]              C                   0                   0            Infinity                   -
+W[F_1,F_4]              C                   0                   0            Infinity                   -
+W[F_1,F_5]              C                   0                   0            Infinity                   -
+W[F_1,F_6]              C                   0                   0            Infinity                   -
+W[F_2,F_1]              C                   0                   0            Infinity                   -
+W[F_2,F_2]              C                   0                   0            Infinity                   -
+W[F_2,F_3]              C                   0                   0            Infinity                   -
+W[F_2,F_4]              C                   0                   0            Infinity                   -
+W[F_2,F_5]              C                   0                   0            Infinity                   -
+W[F_2,F_6]              C                   0                   0            Infinity                   -
+W[F_3,F_1]              C                   0                   0            Infinity                   -
+W[F_3,F_2]              C                   0                   0            Infinity                   -
+W[F_3,F_3]              C                   0                   0            Infinity                   -
+W[F_3,F_4]              C                   0                   0            Infinity                   -
+W[F_3,F_5]              C                   0                   0            Infinity                   -
+W[F_3,F_6]              C                   0                   0            Infinity                   -
+W[F_4,F_1]              C                   0                   0            Infinity                   -
+W[F_4,F_2]              C                   0                   0            Infinity                   -
+W[F_4,F_3]              C                   0                   0            Infinity                   -
+W[F_4,F_4]              C                   0                   0            Infinity                   -
+W[F_4,F_5]              C                   0                   0            Infinity                   -
+W[F_4,F_6]              C                   1                   0            Infinity                   -
+W[F_5,F_1]              C                   1                   0            Infinity                   -
+W[F_5,F_2]              C                   1                   0            Infinity                   -
+W[F_5,F_3]              C                   1                   0            Infinity                   -
+W[F_5,F_4]              C                   1                   0            Infinity                   -
+W[F_5,F_5]              C                   1                   0            Infinity                   -
+W[F_5,F_6]              C                   0                   0            Infinity                   -
+W[F_6,F_1]              C                   0                   0            Infinity                   -
+W[F_6,F_2]              C                   0                   0            Infinity                   -
+W[F_6,F_3]              C                   0                   0            Infinity                   -
+W[F_6,F_4]              C                   0                   0            Infinity                   -
+W[F_6,F_5]              C                   0                   0            Infinity                   -
+W[F_6,F_6]              C                   0                   0            Infinity                   -
+---------------------------------------------------------------------------------------------------------
+
+Constraints 
+Name                 Type            Activity         Lower bound         Upper bound            Marginal
+---------------------------------------------------------------------------------------------------------
+farms[F_1]              L                   0           -Infinity                   0                   -
+farms[F_2]              L                   0           -Infinity                   0                   -
+farms[F_3]              L                   0           -Infinity                   0                   -
+farms[F_4]              L                   0           -Infinity                   0                   -
+farms[F_5]              L                   0           -Infinity                   0                   -
+farms[F_6]              L                   0           -Infinity                   0                   -
+exclusivity[F_1]        L                   1           -Infinity                   1                   -
+exclusivity[F_2]        L                   1           -Infinity                   1                   -
+exclusivity[F_3]        L                   1           -Infinity                   1                   -
+exclusivity[F_4]        L                   1           -Infinity                   1                   -
+exclusivity[F_5]        L                   1           -Infinity                   1                   -
+exclusivity[F_6]        L                   1           -Infinity                   1                   -
+nPlants[F_1]            L                   0           -Infinity                   1                   -
+nPlants[F_2]            L                   0           -Infinity                   1                   -
+nPlants[F_3]            L                   0           -Infinity                   1                   -
+nPlants[F_4]            L                   1           -Infinity                   1                   -
+nPlants[F_5]            L                   1           -Infinity                   1                   -
+nPlants[F_6]            L                   0           -Infinity                   1                   -
+cond1[F_1,F_1]          L                   0           -Infinity                   0                   -
+cond1[F_1,F_2]          L                   0           -Infinity                   0                   -
+cond1[F_1,F_3]          L                   0           -Infinity                   0                   -
+cond1[F_1,F_4]          L                   0           -Infinity                   0                   -
+cond1[F_1,F_5]          L                   0           -Infinity                   0                   -
+cond1[F_1,F_6]          L                   0           -Infinity                   0                   -
+cond1[F_2,F_1]          L                   0           -Infinity                   0                   -
+cond1[F_2,F_2]          L                   0           -Infinity                   0                   -
+cond1[F_2,F_3]          L                   0           -Infinity                   0                   -
+cond1[F_2,F_4]          L                   0           -Infinity                   0                   -
+cond1[F_2,F_5]          L                   0           -Infinity                   0                   -
+cond1[F_2,F_6]          L                   0           -Infinity                   0                   -
+cond1[F_3,F_1]          L                   0           -Infinity                   0                   -
+cond1[F_3,F_2]          L                   0           -Infinity                   0                   -
+cond1[F_3,F_3]          L                   0           -Infinity                   0                   -
+cond1[F_3,F_4]          L                   0           -Infinity                   0                   -
+cond1[F_3,F_5]          L                   0           -Infinity                   0                   -
+cond1[F_3,F_6]          L                   0           -Infinity                   0                   -
+cond1[F_4,F_1]          L                  -1           -Infinity                   0                   -
+cond1[F_4,F_2]          L                  -1           -Infinity                   0                   -
+cond1[F_4,F_3]          L                  -1           -Infinity                   0                   -
+cond1[F_4,F_4]          L                  -1           -Infinity                   0                   -
+cond1[F_4,F_5]          L                  -1           -Infinity                   0                   -
+cond1[F_4,F_6]          L                   0           -Infinity                   0                   -
+cond1[F_5,F_1]          L                   0           -Infinity                   0                   -
+cond1[F_5,F_2]          L                   0           -Infinity                   0                   -
+cond1[F_5,F_3]          L                   0           -Infinity                   0                   -
+cond1[F_5,F_4]          L                   0           -Infinity                   0                   -
+cond1[F_5,F_5]          L                   0           -Infinity                   0                   -
+cond1[F_5,F_6]          L                  -1           -Infinity                   0                   -
+cond1[F_6,F_1]          L                   0           -Infinity                   0                   -
+cond1[F_6,F_2]          L                   0           -Infinity                   0                   -
+cond1[F_6,F_3]          L                   0           -Infinity                   0                   -
+cond1[F_6,F_4]          L                   0           -Infinity                   0                   -
+cond1[F_6,F_5]          L                   0           -Infinity                   0                   -
+cond1[F_6,F_6]          L                   0           -Infinity                   0                   -
+ProductionCap[F_1]      L                   0           -Infinity                   0                   -
+ProductionCap[F_2]      L                   0           -Infinity                   0                   -
+ProductionCap[F_3]      L                   0           -Infinity                   0                   -
+ProductionCap[F_4]      L                 -11           -Infinity                   0                   -
+ProductionCap[F_5]      L                  -6           -Infinity                   0                   -
+ProductionCap[F_6]      L                   0           -Infinity                   0                   -
+equivalence[F_1]        E                   0                   0                   0                   -
+equivalence[F_2]        E                   0                   0                   0                   -
+equivalence[F_3]        E                   0                   0                   0                   -
+equivalence[F_4]        E                   0                   0                   0                   -
+equivalence[F_5]        E                   0                   0                   0                   -
+equivalence[F_6]        E                   0                   0                   0                   -
+Const                   G                  23                  23            Infinity                   -
+---------------------------------------------------------------------------------------------------------
